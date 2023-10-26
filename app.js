a08cf34f40eff9b68aded28a1913d673b9c8e281
khalidelnaghi <khalidelnaghii@gmial.com>
2023-10-26 22:19:13 +0300

    initail commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll(".tab");
+const tabContent = document.querySelectorAll(".content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    tabContent.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabContent[index].classList.add("active");
+
+    let line = document.querySelector(".line");
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
+  });
+});
